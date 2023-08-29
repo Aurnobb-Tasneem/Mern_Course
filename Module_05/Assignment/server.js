460e5a6d08f0802a2b875d49f0d1d0182c25ad29
Tasneem Abdullah Aurnobb <taurnobb@gmail.com>
2023-08-29 23:29:24 +0600

    Update Server.js

--- Module_05/Assignment/server.js
+++ Module_05/Assignment/server.js
@@ -1,23 +1,31 @@
 const http= require("http");
 const fs = require("fs");
-//fdgssdfgs
+//1.
 const app=http.createServer(
     function (req, res){
+        //4.
         if(req.url=="/"){
+            res.writeHead(200,{'Content-Type':'text/html'})
             res.end("This is Home Page");
         }
+        //5.
         else if(req.url=="/about"){
+            res.writeHead(200,{'Content-Type':'text/html'})
             res.end("This is About Page");
         }
+        //6.
         else if(req.url=="/contact"){
+            res.writeHead(200,{'Content-Type':'text/html'})
             res.end("This is Contact Page");
         }
+        //7.
         else if(req.url=="/file-write"){
             fs.writeFile("demo.txt","hello world",(error)=>{
                 if(error){
                     res.writeHead(200,{'Content-Type':'text/html'})
                     res.write("File Write Failed!")
                     console.log("File Write Failed!");
+                  //8.
                     res.end();
                 }
                 else{
@@ -33,6 +41,9 @@ const app=http.createServer(
         }
     }
 
-).listen(3000,()=>{
+).
+    //2.
+listen(3000,()=>{
+//3.
     console.log("listening on port 3000!")
 })
